024665c702b21cadea5aab253be284e78eb1f86d
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-28 14:32:00 -0300

    Pessoa.java class

---
+++ src/Pessoa.java
@@ -0,0 +1,60 @@
+public class Pessoa {
+    /**
+     * Nome de uma pessoa
+     */
+    private String nome;
+
+    /**
+     * Idade de uma pessoa
+     */
+    private int idade;
+
+    /**
+     * Altura de uma pessoa
+     */
+    private Double altura;
+
+    /**
+     * Construtor
+     * 
+     * @param nome   Nome de uma pessoa
+     * @param idade  Idade de uma pessoa
+     * @param altura Altura de uma pessoa
+     */
+    public void Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    /**
+     * Getter de this.nome
+     */
+    public String getNome() {
+        return this.nome;
+    }
+
+    /**
+     * Getter de this.idade
+     */
+    public int getIdade() {
+        return this.idade;
+    }
+
+    /**
+     * Getter de this.altura
+     */
+    public Double getAltura() {
+        return this.altura;
+    }
+
+    /**
+     * Método que retorna uma representação string do objeto
+     */
+    public String toString() {
+        return "{Pessoa:" + 
+            " nome='" + this.nome + "'," +
+            " idade='" + this.idade + "'," +
+            " altura='" + this.altura + "'}";
+    }
+}
